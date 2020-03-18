b086bef5fab133659205060c9f12c1cc32236944
mainzonder <48353743+mainzonder@users.noreply.github.com>
2020-03-18 15:04:03 +0200

    меню боковое

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,17 @@
 $(function() {
   $('.header__slider').slick({
     infinite: true,
-    fade : true
+    fade: true,
+    prevArrow:
+      '<img class="slider-arrows slider-arrows__left" src = "images/arrows-left.svg" alt="стрелка влево"></img>',
+    nextArrow:
+      '<img class="slider-arrows slider-arrows__right" src = "images/arrows-right.svg" alt="стрелка вправо"></img>',
+    asNavFor: '.slider-dots',
+  });
+
+  $('.slider-dots').slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    asNavFor: '.header__slider',
   });
 });
