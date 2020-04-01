4229106bd34b1181846c1f2da7ab63f05853c61d
mainzonder <48353743+mainzonder@users.noreply.github.com>
2020-04-01 07:23:38 +0300

    третий день // все

--- app/js/main.js
+++ app/js/main.js
@@ -32,4 +32,13 @@ $(function () {
     asNavFor: '.surf-slider',
     focusOnSelect: true,
   });
+
+  $('.travel__slider').slick({
+    infinite: true,
+    fade: true,
+    prevArrow:
+      '<img class="slider-arrows slider-arrows__left" src = "images/arrows-left.svg" alt="стрелка влево"></img>',
+    nextArrow:
+      '<img class="slider-arrows slider-arrows__right" src = "images/arrows-right.svg" alt="стрелка вправо"></img>',
+  });
 });
