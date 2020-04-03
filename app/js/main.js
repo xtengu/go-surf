a0de86a73c80cd8083dfee33800c1bd18e8e1bbc
mainzonder <48353743+mainzonder@users.noreply.github.com>
2020-04-03 09:45:25 +0300

    4 день // 1 час // ok

--- app/js/main.js
+++ app/js/main.js
@@ -41,4 +41,38 @@ $(function () {
     nextArrow:
       '<img class="slider-arrows slider-arrows__right" src = "images/arrows-right.svg" alt="стрелка вправо"></img>',
   });
+
+  $(
+    '<div class="quantity-nav"><div class="quantity-button quantity-up"><img src="images/plus.svg"></div><div class="quantity-button quantity-down"><img src="images/minus.svg"></div></div>',
+  ).insertAfter('.quantity input');
+  $('.quantity').each(function () {
+    var spinner = $(this),
+      input = spinner.find('input[type="number"]'),
+      btnUp = spinner.find('.quantity-up'),
+      btnDown = spinner.find('.quantity-down'),
+      min = input.attr('min'),
+      max = input.attr('max');
+
+    btnUp.click(function () {
+      var oldValue = parseFloat(input.val());
+      if (oldValue >= max) {
+        var newVal = oldValue;
+      } else {
+        var newVal = oldValue + 1;
+      }
+      spinner.find('input').val(newVal);
+      spinner.find('input').trigger('change');
+    });
+
+    btnDown.click(function () {
+      var oldValue = parseFloat(input.val());
+      if (oldValue <= min) {
+        var newVal = oldValue;
+      } else {
+        var newVal = oldValue - 1;
+      }
+      spinner.find('input').val(newVal);
+      spinner.find('input').trigger('change');
+    });
+  });
 });
